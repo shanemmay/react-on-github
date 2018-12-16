e489fe0e00158a4f9ea50d21539e6c95d5f7ceb0
shanemmay <shanemarvinmay@hotmail.com>
2018-12-15 18:33:29 -0600

    Updates

---
+++ precache-manifest.157497eedef2f1b879378c104003e68e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "191c370417dd880a8368",
+    "url": "/react-on-github/static/css/main.6e84a459.chunk.css"
+  },
+  {
+    "revision": "191c370417dd880a8368",
+    "url": "/react-on-github/static/js/main.191c3704.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/react-on-github/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "50d902754de63d4e52a0",
+    "url": "/react-on-github/static/js/runtime~main.50d90275.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-on-github/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c719b5b52e55ba191085ec58fe971e71",
+    "url": "/react-on-github/index.html"
+  }
+];
